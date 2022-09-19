4083b937c118d8ac200040a34f8111d61a7799c8
Setimarz108 <66173614+Setimarz108@users.noreply.github.com>
2022-09-19 21:29:20 +0200

    hue picker

--- react-todo/src/components/ThemeSwitcher.jsx
+++ react-todo/src/components/ThemeSwitcher.jsx
@@ -10,20 +10,28 @@ import {
 function ThemeSwitcher() {
   const [isColorPicking, setIsColorPicking] = useState(false);
   const [theme, setTheme] = useState("light");
- 
+  const [hue, setHue] = useState("240");
 
   return (
     <aside className={styles.wrapper}>
       {isColorPicking ? (
         <>
           <button
-          className={`btn ${styles.close}`}
-          aria-label="Close color picking mode"
-          onClick={()=> setIsColorPicking(false)}
+            className={`btn ${styles.close}`}
+            aria-label="Close color picking mode"
+            onClick={() => setIsColorPicking(false)}
           >
-            <XMarkIcon/>
-            </button>
-          <input type="range" />
+            <XMarkIcon />
+          </button>
+          <input 
+          className={styles.picker} 
+          type="range"
+          min="0"
+          max="360"
+          aria-label="Change color theme slider"
+          value={hue}
+          onInput={(e) => setHue(e.target.value)}
+          />
         </>
       ) : (
         <div className={styles.btns}>
@@ -38,9 +46,10 @@ function ThemeSwitcher() {
             {theme === "dark" ? <SunIcon /> : <MoonIcon />}
           </button>
           <button
-           className="btn"
-           aria-label="Enable color picking mode"
-           onClick={ () => setIsColorPicking(true)}>
+            className="btn"
+            aria-label="Enable color picking mode"
+            onClick={() => setIsColorPicking(true)}
+          >
             <SwatchIcon />
           </button>
         </div>
